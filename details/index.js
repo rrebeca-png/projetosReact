9002e62ad7ea2625ea4d37935d059c6e99f70c86
Rebeca Nalin <119748427+rrebeca-png@users.noreply.github.com>
2023-11-18 08:43:15 -0300

    Add files via upload
    pagina de detalhes

---
+++ details/index.js
@@ -0,0 +1,11 @@
+import { Container } from "./style";
+
+function Details(){
+    return(
+        <Container>
+            <div className="movie">
+                <h1>Detalhes do Filme</h1>
+            </div>
+        </Container>
+    )
+}
