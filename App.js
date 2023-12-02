0dd5cd63590297403a675a2a7ed1a3d2ebf99e3a
Rebeca Nalin <119748427+rrebeca-png@users.noreply.github.com>
2023-12-02 12:19:46 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import Header from './header/Header'
+import Home from './home/Home'
+
+function App() {
+  return (
+    <div>
+      
+       <Header>
+       </Header>
+       
+       <Home>
+       </Home>
+       
+    </div>
+  );
+}
+
+export default App;
+
